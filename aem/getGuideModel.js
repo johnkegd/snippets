3810868afa900c64f944fd12508adedf2c04932c
John Garcia <johnkegd@gmail.com>
2022-08-22 17:17:05 +0200

    [AEM] - added aem guideView getter snipper tool

--- aem/getGuideModel.js
+++ aem/getGuideModel.js
@@ -1,18 +1,34 @@
-(function () {
-    var guideView;
+(function() {
+    const viewName = "guideView";
     var guideModel;
-    if (xfalib.$) {
-        guideView = xfalib.$($0).data("guideView");
-    } else {
-        guideView = $($0).data("guideView");
-    }
+    var guideJqueryContext = getGuideJqueryContext();
+    var guideFieldNode = getGuideFieldNode();
+    var guideView = xfalib.$ ? xfalib.$(guideFieldNode).data(viewName) : $(guideFieldNode).data(viewName);
 
     if (guideView) {
-        guideModel = guideView._model;
-        window.guideModelTarget = guideModel;
-        console.log(guideModel);
+        window.guideModel = guideView._model;
+        window.guideView = guideView;
+        console.log("Type guideModel in console to manipulate the guide :", window.guideModel);
+        console.log("Current component value: ", window.guideModel.value);
     } else {
         console.debug("not guideView found in current $0 selector target: ", $0);
+    }
+
+    function getGuideJqueryContext() {
+        if (xfalib.$) {
+            return xfalib.$;
+        } else {
+            return $;
+        }
+    }
+
+    function getGuideFieldNode() {
+        return $0.classList.contains("guideFieldNode") ? $0 : getParentElement();
+    }
+
+    function getParentElement() {
+        var parentsUntilGuideField = $($0).parentsUntil(".guidefield");
+        return parentsUntilGuideField[parentsUntilGuideField.length - 1];
     }
 
 }());
